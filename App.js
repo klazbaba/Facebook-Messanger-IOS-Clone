1d83bbc378f15e4219ab8104924b83ae802fc58e
klazbaba <giwaklaz@gmail.com>
2019-04-30 12:17:32 +0100

    completed task

--- App.js
+++ App.js
@@ -1,49 +1,143 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { FlatList, StyleSheet, Text, View, SafeAreaView, Animated, TextInput } from 'react-native';
+import { Icon } from 'native-base';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+  state = {
+    showArrow: false
+  };
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  DATA = [
+    { user: 'me', message: 'Hi' },
+    { user: 'you', message: 'Hi' },
+    { user: 'me', message: 'How is family?' },
+    { user: 'you', message: 'Good.' },
+    { user: 'me', message: 'I tried reaching you yesterday but to no avail.' },
+    { user: 'you', message: 'That is true. I was in a meeting throughtout the day.' },
+    { user: 'me', message: 'OK' },
+    { user: 'you', message: "So, what's up?" },
+    {
+      user: 'me',
+      message: 'Nothing much. Just wanted to ask you if you went for lectures yesterday before.'
+    },
+    { user: 'you', message: 'OK.\nHow are your folks?' },
+    { user: 'me', message: 'They are doing great walhamdulillaah.' },
+    { user: 'you', message: 'Alright, do extend my regards to them.' },
+    { user: 'me', message: 'OK. I shall do that, take care.' },
+    { user: 'you', message: 'Yep, take care.' }
+  ];
+  animatedValue = new Animated.Value(150);
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
+    const { showArrow } = this.state;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <KeyboardAwareScrollView
+          style={styles.container}
+          contentContainerStyle={{ justifyContent: 'center' }}
+          keyboardDismissMode="on-drag"
+        >
+          <View style={{ marginBottom: 16 }}>
+            <FlatList
+              data={this.DATA}
+              renderItem={this.renderItem}
+              keyExtractor={(item, index) => String(index)}
+              contentContainerStyle={{ paddingHorizontal: 16 }}
+            />
+
+            <View style={styles.bottomWrapper}>
+              <Animated.View style={[styles.iconsWrapper, { width: this.animatedValue }]}>
+                {!showArrow ? (
+                  <View style={{ flexDirection: 'row' }}>
+                    <Icon name="grid" type="Entypo" style={styles.icon} />
+                    <Icon name="camera" type="Entypo" style={styles.icon} />
+                    <Icon name="picture" type="AntDesign" style={styles.icon} />
+                    <Icon name="mic" type="Entypo" style={styles.icon} />
+                  </View>
+                ) : (
+                  <Icon name="right" type="AntDesign" style={styles.icon} />
+                )}
+              </Animated.View>
+
+              <TextInput
+                placeholder="Aa"
+                style={styles.textInput}
+                placeholderTextColor={'grey'}
+                onFocus={this.decrease}
+                onBlur={this.expand}
+              />
+              <Icon name="smile" type="FontAwesome5" style={styles.icon} />
+              <Icon name="thumbs-up" type="FontAwesome" style={styles.icon} />
+            </View>
+          </View>
+        </KeyboardAwareScrollView>
+      </SafeAreaView>
     );
   }
+
+  renderItem = ({ item }) => {
+    return (
+      <View
+        style={[
+          styles.messageWrapper,
+          { alignSelf: item.user === 'me' ? 'flex-start' : 'flex-end' },
+          { backgroundColor: item.user === 'me' ? 'lightgrey' : '#4267b2' }
+        ]}
+      >
+        <Text style={{ color: item.user === 'you' ? 'white' : undefined }}>{item.message}</Text>
+      </View>
+    );
+  };
+
+  decrease = () => {
+    Animated.timing(this.animatedValue, {
+      toValue: 30
+    }).start();
+    this.setState({ showArrow: true });
+  };
+
+  expand = () => {
+    Animated.timing(this.animatedValue, {
+      toValue: 150
+    }).start();
+    this.setState({ showArrow: false });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
+    paddingVertical: 16
+  },
+  messageWrapper: {
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginVertical: 8,
+    padding: 8,
+    borderRadius: 8,
+    maxWidth: 300
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  bottomWrapper: {
+    flexDirection: 'row',
+    marginBottom: 8,
+    paddingHorizontal: 8
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  iconsWrapper: {
+    flexDirection: 'row',
+    width: 150
   },
+  icon: {
+    color: '#4267b2',
+    marginRight: 8
+  },
+  textInput: {
+    backgroundColor: 'lightgrey',
+    borderRadius: 16,
+    flex: 1,
+    paddingHorizontal: 8,
+    color: 'grey',
+    marginRight: 8
+  }
 });
